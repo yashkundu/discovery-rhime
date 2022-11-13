ada421bbcdf3acb6f4dd5f8fc3a8f7849789e404
yashkundu <yashasvik107@gmail.com>
2022-11-13 17:51:24 +0530

    done

--- build/controllers/register.d.ts
+++
@@ -1,4 +0,0 @@
-import { Etcd3 } from "etcd3";
-import { config, options, service } from "../interfaces";
-declare const register: (serviceName: service, config: config, opts: options, etcd: Etcd3) => Promise<void>;
-export { register };
--- build/controllers/watch.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference types="node" />
-import { service } from "../interfaces";
-import { Etcd3 } from "etcd3";
-import { EventEmitter } from "events";
-declare const watchOne: (serviceName: service, etcd: Etcd3) => Promise<EventEmitter>;
-declare const watchPrefix: (prefix: string, etcd: Etcd3) => Promise<EventEmitter>;
-export { watchOne, watchPrefix };
